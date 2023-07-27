1e06769edb64b53a09681b2a5e04254cb084e0e6
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-27 18:37:33 -0500

    Got index.js prompts to work and create logo, testing to see if logo appears correctly

--- index.js
+++ index.js
@@ -1,5 +1,76 @@
-const CLI = require('./lib/cli.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Square, Circle} = require('./lib/shapes.js')
+const SVG = require('./lib/svg.js');
 
-const cli = new CLI();
+function questions() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What text do you want to display in the logo? (Enter up to  3 characters)',
+                name: 'text',
+            },
+            {
+                type: 'input',
+                message: 'What color do you want for the text in the logo? (Enter color keyword or a hexadecimal number)',
+                name: 'textColor',
+            },
+            {
+                type: 'list',
+                message: 'Which shape would you prefer for the logo?',
+                name: 'shape',
+                choices: ['Triangle', 'Square', 'Circle']
+            },
+            {
+                type: 'input',
+                message: 'What color do you want for the shape? (Enter color keyword or a hexadecimal number)',
+                name: 'shapeColor',
+            },
+        ])
+        .then((response) => {
+            // console.log(response)
+            generateSVG(response)
 
-cli.questions();
+            if (response.text.length > 3) {
+                console.log('Your text cannot be more than 3 characters.');
+                questions();
+            } else {
+                writeToFile('logo.svg', svgString)
+            }
+        })
+}
+
+
+let svgString = "";
+
+function generateSVG(response) {
+
+    let shapeOption;
+
+    if (response.shape === 'Triangle') {
+        shapeOption = new Triangle();
+    } else if (response.shape === 'Square') {
+        shapeOption = new Square()
+    } else if (response.shape === 'Circle') {
+        shapeOption = new Circle()
+    } else {
+        console.log("Invalid shape!")
+    }
+
+    shapeOption.setColor(response.shapeColor);
+    console.log(shapeOption)
+
+
+    const svg = new SVG();
+    svg.setTextString(response.text, response.textColor);
+    svg.setShapeOption(shapeOption)
+    svgString = svg.render()
+}
+
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, (err) =>
+    err ? console.log(err) : console.log('Logo created!!'))
+}
+
+questions()
